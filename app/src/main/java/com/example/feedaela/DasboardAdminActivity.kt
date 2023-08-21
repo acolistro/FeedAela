48bfc0088ba8c00ca2bbad91c02152baa361238c
alex <arcolistro@gmail.com>
2023-08-21 00:45:46 -0700

    Add login, register and dashboard activities

---
+++ app/src/main/java/com/example/feedaela/DasboardAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.example.feedaela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DasboardAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dasboard_admin)
+    }
+}
