9014149a45bcc0ec7d4984bd0d583bc4e2d4be62
alex <arcolistro@gmail.com>
2023-08-20 20:51:02 -0700

    Add services.json from firebase

--- app/src/main/java/com/example/feedaela/MainActivity.kt
+++ app/src/main/java/com/example/feedaela/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.feedaela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.feedaela.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //handle click, login
+        binding.loginBtn.setOnClickListener {
+            //TODO
+        }
+
+        //handle click, sip and continue to main screen
+        binding.skipBtn.setOnClickListener {
+            //TODO
+        }
+
+
     }
 }
